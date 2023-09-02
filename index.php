3d813e63b683b10ad069176459e929a995c866d7
MonocledDagger <Dictus1@sbcglobal.net>
2023-09-02 15:33:06 -0500

    Index Start

---
+++ index.php
@@ -0,0 +1,29 @@
+!<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Jimmy's Homepage</title>
+</head>
+<body>
+<header>
+    <h1>PHP Class - Jimmy's Website</h1>
+</header>
+<nav>
+    <ul>
+        <li><a href="/">Homepage</a></li>
+        <li><a href="/">Loop</a></li>
+        <li><a href="/">Countdown</a></li>
+        <li><a href="/">Other</a></li>
+    </ul>
+</nav>
+<main>
+    <p> Information</p>
+</main>
+<footer>
+    <h6>&copy; 2023 Jimmy Dictus</h6>
+</footer>
+</body>
+</html>
