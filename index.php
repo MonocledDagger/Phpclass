dd7f626153bae17b9220aa3539708b64a8a623e4
MonocledDagger <Dictus1@sbcglobal.net>
2023-09-03 11:26:27 -0500

    Index Start

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-!<!doctype html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Jimmy's Homepage</title>
+    <link type="text/css" rel="stylesheet" href="css/base.css">
 </head>
 <body>
 <header>
@@ -20,10 +21,18 @@
     </ul>
 </nav>
 <main>
-    <p> Information</p>
+    <p> Jimmy is a pretty cool guy. Here's a picture of him</p>
+    <img src="images/Me.jpg" alt="Picture of Jimmy" width="50%" height="50%">
+    <br>
+    <p>Yeah, that's really something. Wow. Just some quick facts, in 1994, Jimmy was born and just 4 years later he became the youngest man alive to beat a tiger in a boxing match. The tiger lost by disqualification after ignoring the referee one too many times. The tiger, named Francis Ju Pai, became one of Jimmy's best friends before he was skinned for his pelt in 2003. The culprits are still at large. Jimmy searches night and day for the murderers of his best friend. </p>
+
+    <br>
+
+    <p>In his off time,Jimmy plays video games, watches Let's plays, reads comic books and is currently learning to code.</p>
 </main>
 <footer>
-    <h6>&copy; 2023 Jimmy Dictus</h6>
+    <p>&copy; 2023 Jimmy Dictus</p>
+
 </footer>
 </body>
 </html>
