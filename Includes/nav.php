4e69012b4932ae6b070bd36d6ec8b5641274d8bb
MonocledDagger <Dictus1@sbcglobal.net>
2023-09-08 15:22:38 -0500

    Chapter 3

--- Includes/nav.php
+++ Includes/nav.php
@@ -1,6 +1,6 @@
 <ul>
     <li><a href="/">Homepage</a></li>
-    <li><a href="/">Loop</a></li>
+    <li><a href="/Loops/loops.php">Loop</a></li>
     <li><a href="/">Countdown</a></li>
     <li><a href="/">Other</a></li>
 </ul>
