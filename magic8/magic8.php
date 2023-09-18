67cf1b28840fe89d70d3c026116b750496ef8ef9
MonocledDagger <Dictus1@sbcglobal.net>
2023-09-18 15:12:45 -0500

    Chapter 4

--- magic8/magic8.php
+++ magic8/magic8.php
@@ -1,53 +1,3 @@
-<?php
-session_start();
-
-if(isset($_POST["txtQuestion"])) {
-    $question = $_POST["txtQuestion"];
-}else{
-    $question = "";
-}
-
-    if(isset($_SESSION["PrevQuest"])){
-        $PrevQuest = $_SESSION["PrevQuest"];
-    }else{
-        $PrevQuest = "";
-    }
-
-    $responses = array();
-    $responses[0] ="Ask Again Later";
-    $responses[1] = "Yes";
-    $responses[2] = "No";
-    $responses[3] = "It Appears to be so";
-    $responses[4] = "Reply is Hazy. Try Again";
-    $responses[5] = "Yes, definitely";
-    $responses[6] = "What is it you really want to know?";
-    $responses[7] = "Outlook is good";
-    $responses[8] = "My Sources say no";
-    $responses[9] = "Signs point to Yes";
-    $responses[10] = "Don't Count on it";
-    $responses[11] = "Cannot Predict Now";
-    $responses[12] = "As I see it, yes";
-    $responses[13] = "Better Not Tell You now";
-    $responses[14] = "Concentrate and Ask Again";
-
-    if($question="") {
-        $answer = "Ask Me A Question";
-    }elseif(substr($question, -1) != "?"){
-        $answer = "Ask me with a question mark";
-    }elseif($PrevQuest == $question){
-        $answer ="Please Ask a NEW Question";
-    }else {
-        $iResponse = mt_rand(0,14);
-        $answer = $responses[$iResponse];
-        $_SESSION["PrevQuest"] = $question;
-    }
-    exit();
-
-
-
-
-
-?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -68,11 +18,11 @@ if(isset($_POST["txtQuestion"])) {
 <main>
     <h2>Magic 8 Ball~!</h2>
     <br />
-    <marquee><?=$answer?>></marquee>
+
     <br />
     <p>Ask A Question:<br />
     <form method="post" action="magic8.php">
-    <input type="text" name="txtQuestion" id="txtQuestion" value=<?$question?>></p>
+
     <input type ="Submit" value="Ask the 8-Ball">
     </form>
 </main>
